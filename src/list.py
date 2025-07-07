859757d8afa2629e057056496c21a777b2c42231
Miguel Angel Sanchez Martinez <masanmar.s70@gmail.com>
2025-07-06 21:11:42 -0600

    Commit de prueba

--- src/list.py
+++ src/list.py
@@ -6,7 +6,7 @@ import todoList
 def list(event, context):
     # fetch all todos from the database
     result = todoList.get_items()
-    # create a response
+    # create a response.
     response = {
         "statusCode": 200,
         "body": json.dumps(result, cls=decimalencoder.DecimalEncoder)
